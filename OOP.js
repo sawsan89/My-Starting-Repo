a62302831b627f8aa45a952263c26e6417093d3c
sawsan89 <sawsan.h.m.89@gmail.com>
2020-04-23 14:49:21 +0300

    start function

--- OOP.js
+++ OOP.js
@@ -28,3 +28,14 @@ var eject = function(){
     return type;
   } return "no taost";
 };
+
+var start = function(){
+    if (this.count === 0){
+   if (this.toast !== "" && this.toast !== undefined && this.toast !== NaN){this.clear = setTimeout(function() {console.log('Ding!');}, 8000);
+   this.count = this.count + 1;
+   this.toast = this.toast + " is Toasted!"}
+   else return "please add toast!"}
+   else if (this.toast !== "" && this.toast !== undefined && this.toast !== NaN){this.clear = setTimeout(function() {console.log('Ding!');}, 8000);
+   this.toast = this.toast.replace(" is Toasted!" ," is Burned!")}
+   else return "please add toast!"
+};
